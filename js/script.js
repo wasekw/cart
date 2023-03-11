0ee3d229e0e597cb739c5ac0662675a97312cc7f
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-03-11 12:12:19 +0200

    Start project

---
+++ js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    let products = document.querySelectorAll('.product');
+    let openBtn = document.querySelector('.open');
+    let byeBtns = document.querySelectorAll('button');
+    console.log(byeBtns);   
+
+    function createCart() {
+        cart = document.createElement('div');
+        field = document.createElement('div');
+        heading = document.createElement('h2');
+        button = document.createElement('button');
+        
+    }
+
+});
+
